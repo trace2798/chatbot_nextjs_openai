1604b3cdecb9da3a850a3582ee3b1d068515f6c2
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-02 14:49:56 +0530

    added project

--- src/app/helper/constants/book-data.ts
+++ src/app/helper/constants/book-data.ts
@@ -10,8 +10,23 @@ export const bookData = `
 </url>
 <url>
 <url>https://bohniman-systems-test.vercel.app/</url>
-<desc>An overview about the company</desc>
+<desc>Bohniman Systems Pvt. Ltd. is an Integrated Software, web development and Data Management solutions and service providing company in India. The company’s corporate head office is located in Guwahati, the capital city of the state of Assam in the north eastern region of India. Over the period of last 24 (Twenty-four) years of operation, the company has a successful track record in executing various sizes of projects involving development and implementation of integrated software solutions and high-quality data management services to both Government and private sector clients.</desc>
+<title>Projects we have worked on</title>
+<desc>Few of our significant projects are Digitized Legacy Data Development, e-ILP (Inner Line Permit), APART GIS Mapping and Decision Support Tool and Assam Agricultural Competitiveness Project GIS</desc>
+<title>Digitized Legacy Data Development</title>
+<desc>We successfully completed a project to provide end-to-end digitized Legacy Data Development for NRC, Assam, handling 170 million hits, 14 million legacy data downloads, and scanning 0.63 million legacy records over one year. We also designed a search portal for citizens to access their legacy data in multiple languages and achieved imaging of original NRC schedules and electoral roll papers with metadata transliterated into multiple languages.</desc>
+<title> e-ILP (Inner Line Permit)</title>
+<desc>We developed the e-ILP project to automate the process of issuing Inner Line Permits (ILPs) to Indian citizens who wish to visit Arunachal Pradesh. The project was developed for the Political Department of Arunachal Pradesh and was launched on July 1, 2017. Since then, over 45,449 applicants have used the e-ILP system. The e-ILP system has been recognized for its innovation and efficiency. It has won several awards, including the e-Northeast Award (2013), special mention in the 6th e-Northeast Award Summit (2016), Indian Express Award (2017) for category in Connect to Unconnect Citizen, and 49th SKOCH Award (2017) for Category in Smart Governance. The e-ILP system is a valuable tool for promoting tourism in Arunachal Pradesh. It makes it easier and more convenient for Indian citizens to obtain an ILP, which in turn encourages more people to visit the state. The e-ILP system also helps to improve the efficiency of the government's ILP process, which frees up resources that can be used to improve other aspects of the state's tourism industry. If you are an Indian citizen who is planning to visit Arunachal Pradesh, I encourage you to use the e-ILP system. It is a quick, easy, and convenient way to obtain an ILP.</desc>
+
+<title>APART GIS Mapping</title>
+<desc>The APART GIS Mapping and Decision Support Tool (AGMDST) is a software application that was developed to digitally map and plot the block/village prioritized clusters of the Assam Agribusiness and Rural Transformation Project (APART) in Google Maps. The project aims to increase value-added and improve resilience in the production and processing of selected agriculture commodities, focusing on small farmers and agro-entrepreneurs in targeted districts. The project is owned by the World Bank Funded ARIAS Society.</desc>
+
+<title>Assam Agricultural Competitiveness Project GIS</title>
+<desc>Developed a GIS tool to help users locate shallow tube wells (STWs) under the Assam Agricultural Competitiveness Project (AACP) on a Google map by districts, blocks, and villages. It was a World Bank-financed project, for the Assam Rural Infrastructure and Agricultural services (ARIAS) Society. The basic objective of the tool is to plot the GPS points for those installed STWs in Google Maps for graphical presentation. Data entry, processing, cleaning, and plotting of GPS points of installed STWs have been incorporated in Google Maps, as well the data of the beneficiary group with photos and also water quality data at the GPS point. The tool is a valuable resource for AACP stakeholders, including government officials, project implementers, and beneficiaries. It will help improve access to safe water in Assam by making it easier to identify and map STWs. The tool will also help to monitor the performance of the AACP and ensure that it is meeting its objectives. The development of the GIS tool is a significant achievement for the ARIAS Society. It demonstrates the Society's commitment to using innovative technologies to improve the lives of the people of Assam.</desc>
+
+
 </url>
+
 <url>
 <loc>https://bohniman-systems-test.vercel.app/services</loc>
 <desc>An overview about the services we provide</desc>
@@ -31,8 +46,7 @@ export const bookData = `
     <services>
     <loc>Support Consultancy</loc>
     <desc>We provide you with the highest level of expertise to support your development project every step of the way, from initial design to final deployment.</desc>
-    </services>
-    
+    </services>   
 </content>
 </url>
 <url>
