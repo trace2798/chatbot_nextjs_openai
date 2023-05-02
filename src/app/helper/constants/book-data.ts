02c726608b2a75982f45c859f4fcbc8984a56a22
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-05-02 14:38:23 +0530

    changed data

--- src/app/helper/constants/book-data.ts
+++ src/app/helper/constants/book-data.ts
@@ -1,12 +1,15 @@
 export const bookData = `
 <desc>My name is Sasha</desc>
-<desc>You can contact Us on whatsApp at 456789023 or visit us at $a, kundalini Plaza, Panbazar.</desc>
+<url>https://bohniman-systems-test.vercel.app/contact</url>
+<desc>You can contact Us on whatsApp at 456789023 or visit us at 4a, kundalini Plaza, Panbazar.</desc>
+<url>https://bohniman-systems-test.vercel.app/in-the-news</url>
+<desc>A cumulative list of news articles where our company Bohniman Systems has been mentioned.</desc>
 <url>
 <loc>https://bohniman-systems-test.vercel.app/</loc>
 <desc>Your Company website</desc>
 </url>
 <url>
-<loc>https://bohniman-systems-test.vercel.app/</loc>
+<url>https://bohniman-systems-test.vercel.app/</url>
 <desc>An overview about the company</desc>
 </url>
 <url>
@@ -14,17 +17,22 @@ export const bookData = `
 <desc>An overview about the services we provide</desc>
 <content>
     <services>
-    <url>https://bohniman-systems-test.vercel.app/services</url>
-    <desc>A book about travelling the himalayas</desc>
-    <price>45.17</price>
-    <availability>in stock</availability>
-    </book>
-    <book>
-    <url>https://books.toscrape.com/catalogue/full-moon-over-noahs-ark-an-odyssey-to-mount-ararat-and-beyond_811/index.html</url>
-    <desc>A book by Rick Ansonson about his journey to Mount Ararat</desc>
-    <price>49.43</price>
-    <availability>in stock</availability>
-    </book>
+    <loc>Enterprise Software & Solutions</loc>
+    <desc>Bohniman offers standalone Scrum project management services and uses this methodology to run its development projects.</desc>
+    </services>
+    <services>
+    <loc>Data Handling</loc>
+    <desc>Legacy Data, Data Survey, Data Migration, Data Analytics, Data Architecture</desc>
+    </services>
+    <services>
+    <loc>Transliteration</loc>
+    <desc>Expertise in transliterating large data sets to and from English, Assamese, Bengali, Hindi etc. Successfully transliterated more than 100 million data till date.</desc>
+    </services>
+    <services>
+    <loc>Support Consultancy</loc>
+    <desc>We provide you with the highest level of expertise to support your development project every step of the way, from initial design to final deployment.</desc>
+    </services>
+    
 </content>
 </url>
 <url>
@@ -33,15 +41,21 @@ export const bookData = `
 <content>
     <product>
     <url>https://bohniman-systems-test.vercel.app/products/the-dancing-bull</url>
-    <desc>A software to manage a restaurant.</desc>
+    <desc>A software to manage a restaurant. It does the following Web panel to manage the menu & prices, Reports & analytics to study sales, Exciting website for customers to buy & bidding items, Bidding improves customers involvement & repetition, Integration with your existing POS, Improved management visibility</desc>
     <price>47.87</price>
     <availability>in stock</availability>
     </product>
     <product>
     <url>https://bohniman-systems-test.vercel.app/products/lab-care</url>
-    <desc>A Hospital Management Software.</desc>
+    <desc>A Hospital Management Software it does the following Reception & Registration, Instant Billing & Payments, Smarter Report Formats, Customer records, Data retrieval and document management, Complete laboratory work flow management</desc>
     <price>On request</price>
     <availability>in stock</availability>
+    </product>
+    <product>
+    <url>https://bohniman-systems-test.vercel.app/contact</url>
+    <desc>Custom Software based on your requirement. Contact us for more details</desc>
+    <price>Depends on the project</price>
+    <availability>Currently Taking projects</availability>
     </product>
     // <book>
     // <url>https://books.toscrape.com/catalogue/the-lab-bull/index.html</url>
@@ -51,4 +65,4 @@ export const bookData = `
     // </product>
 </content>
 </url>
-`
+`;
--- src/app/helper/constants/chatbot-prompt.ts
+++ src/app/helper/constants/chatbot-prompt.ts
@@ -11,6 +11,9 @@ Only include links in markdown format.
 Example: 'You can browse our books [here](https://www.example.com/books)'.
 Other than links, use regular text.
 
+When a customer asks about our products, provide url of product page in markdown format.
+When a customer asks about individual products, provide url of individual product  in markdown format.
+When a customer asks for review provide the news page.
 Refuse any answer that does not have to do with the company or its content.
 Provide short, concise answers.
 `
